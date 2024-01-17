4d983981f8a134ebcbdd0292667b1f17d1f64650
Kevin Maquis <kevin.maquis@udla.edu.ec>
2024-01-16 23:08:50 -0500

    Agregamos valores maximos preestablecidos y definimos valores y variables que usaremos en el ejercicio

--- InformacionAlumnos.c
+++ InformacionAlumnos.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
-
+#include <string.h>
 //Kevin Maquis
+
+#define MaximoAlumnos 50
+#define MaximoNombre 50
+#define MaximoDireccion 50
+#define MaximoCarrera 50
 
 
 int main (){
 
-    int Matricula;
+    int matricula;
+    char nombre[MaximoNombre];
+    char direccion[MaximoDireccion];
+    char carrera[MaximoCarrera];
+    float promedio;
 
 
 
