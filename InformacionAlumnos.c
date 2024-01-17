f610a09a62d6b2dc13340946c58e3725ec2894e0
Kevin Maquis <kevin.maquis@udla.edu.ec>
2024-01-16 23:41:57 -0500

    Creamos la funcion fflush y ademas de eso ya creamos el menu de interaccion para acceder a cada uno de los apartados del programa segun requiera el usuario

--- InformacionAlumnos.c
+++ InformacionAlumnos.c
@@ -16,6 +16,40 @@ struct InformacionAlumnos // Creamos una extructura para acceder de manera mas p
     float promedio;
 };
 
+void ingresarInformacion(struct InformacionAlumnos alumnos[], int *CantAlumnos);
+void mostrarInformacion(struct InformacionAlumnos alumnos[], int CantAlumnos);
+
+int main() {
+    struct InformacionAlumnos alumnos[MaximoAlumnos];
+    int CantAlumnos = 0;
+    int opcion;
+    do {
+        // Creacion del menu de opciones.
+        printf("\nMenu De Seleccion:\n");
+        printf("1. Ingresar nuevos\n");
+        printf("2. Mostrar guardados\n");
+        printf("3. Salir\n");
+        printf("Seleccione una opcion: ");
+        scanf("%d", &opcion);
+
+        fflush(stdin);  
+        // Limpiar el bufer después de cada lectura Para evitar que de errores de memoria 
+        switch (opcion) {
+            case 1:
+                ingresarInformacion(alumnos, &CantAlumnos);
+                break;
+            case 2:
+                mostrarInformacion(alumnos, CantAlumnos);
+                break;
+            case 3:
+                printf("Saliendo del programa.\n");
+                break;
+            default:
+                printf("Opcion no valida. Intente de nuevo.\n");
+        }
+    } while (opcion != 3);
+    return 0;
+}
 // Comenzamos creando las funciones que usaremos para determinar que vamos a ingresar dependiendo de la opcion que se realice cuando realicemos el menu"
 // En este caso es el ingreso de datos para llenar
 void ingresarInformacion(struct InformacionAlumnos alumnos[], int *CantAlumnos)
@@ -24,12 +58,19 @@ void ingresarInformacion(struct InformacionAlumnos alumnos[], int *CantAlumnos)
     {
         printf("\nIngrese la matricula correspondiente: ");
         scanf("%d", &alumnos[*CantAlumnos].matricula);
-        printf("Ingrese el nombre de este alumno: ");       
+        
+        printf("Ingrese el nombre de este alumno: ");
+        fflush(stdin);//Aqui agregamos la funcion fflush para limpiar el buffer y no nos marque error con las funciones gets y puts
         gets(alumnos[*CantAlumnos].nombre);
-        printf("Ingrese la direccion: ");        
+
+        printf("Ingrese la direccion: ");
+        fflush(stdin);
         gets(alumnos[*CantAlumnos].direccion);
+
         printf("Ingrese el nombre carrera : ");
+        fflush(stdin);
         gets(alumnos[*CantAlumnos].carrera);
+
         printf("Ingrese el promedio de la carrera: ");
         scanf("%f", &alumnos[*CantAlumnos].promedio);
 
@@ -47,7 +88,6 @@ void mostrarInformacion(struct InformacionAlumnos alumnos[], int CantAlumnos)
     if (CantAlumnos > 0)
     {
         printf("\nInformacion de los alumnos:\n");
-
         for (int i = 0; i < CantAlumnos; i++)
         {
             printf("\nAlumno %d:\n", i + 1);
