afc80bb449b1a7ded1072dfec740bd9270cb2fc2
Kevin Maquis <kevin.maquis@udla.edu.ec>
2024-01-16 23:33:40 -0500

    Se realizo otra funcion donde el usuario va a poder llamar para que muestre el programa la informacion de los alumnos si es que estos estan ya ingresados y hay datos

--- InformacionAlumnos.c
+++ InformacionAlumnos.c
@@ -1,14 +1,13 @@
 #include <stdio.h>
 #include <string.h>
-//Kevin Maquis
+// Kevin Maquis
 
 #define MaximoAlumnos 50
 #define MaximoNombre 50
 #define MaximoDireccion 50
 #define MaximoCarrera 50
 
-
-struct InformacionAlumnos//Creamos una extructura para acceder de manera mas puntual a los datos y poder trabjar de mejor manera
+struct InformacionAlumnos // Creamos una extructura para acceder de manera mas puntual a los datos y poder trabjar de mejor manera
 {
     int matricula;
     char nombre[MaximoNombre];
@@ -17,11 +16,50 @@ struct InformacionAlumnos//Creamos una extructura para acceder de manera mas pun
     float promedio;
 };
 
-//Comenzamos creando las funciones que usaremos para determinar que vamos a ingresar dependiendo de la opcion que se realice cuando realicemos el menu"
-//En este caso es el ingreso de datos para llenar
-void ingresarInformacion(struct InformacionAlumnos alumnos[], int *CantAlumnos) {
-    if (*CantAlumnos < MaximoAlumnos) {
-        printf("\nIngrese la matricula del nuevo alumno: ");
+// Comenzamos creando las funciones que usaremos para determinar que vamos a ingresar dependiendo de la opcion que se realice cuando realicemos el menu"
+// En este caso es el ingreso de datos para llenar
+void ingresarInformacion(struct InformacionAlumnos alumnos[], int *CantAlumnos)
+{
+    if (*CantAlumnos < MaximoAlumnos)
+    {
+        printf("\nIngrese la matricula correspondiente: ");
         scanf("%d", &alumnos[*CantAlumnos].matricula);
+        printf("Ingrese el nombre de este alumno: ");       
+        gets(alumnos[*CantAlumnos].nombre);
+        printf("Ingrese la direccion: ");        
+        gets(alumnos[*CantAlumnos].direccion);
+        printf("Ingrese el nombre carrera : ");
+        gets(alumnos[*CantAlumnos].carrera);
+        printf("Ingrese el promedio de la carrera: ");
+        scanf("%f", &alumnos[*CantAlumnos].promedio);
+
+        (*CantAlumnos)++;
+        printf("\tDatos guardados correctamente.\n");
     }
-}  
+    else
+    {
+        printf("Se alcanzo el limite.\n");
+    }
+}
+
+void mostrarInformacion(struct InformacionAlumnos alumnos[], int CantAlumnos)
+{
+    if (CantAlumnos > 0)
+    {
+        printf("\nInformacion de los alumnos:\n");
+
+        for (int i = 0; i < CantAlumnos; i++)
+        {
+            printf("\nAlumno %d:\n", i + 1);
+            printf("Matricula: %d\n", alumnos[i].matricula);
+            printf("Nombre: %s\n", alumnos[i].nombre);
+            printf("Direccion: %s\n", alumnos[i].direccion);
+            printf("Carrera: %s\n", alumnos[i].carrera);
+            printf("Promedio: %.2f\n", alumnos[i].promedio);
+        }
+    }
+    else
+    {
+        printf("No hay datos para mostrar.\n");
+    }
+}
